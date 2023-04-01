ee217493bf476314af245c049ac3655085f7f19f
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-04-01 11:38:32 +0200

    created entrypoint to app

---
+++ Bookkeeper/Bookkeeper/Entrypoint.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookkeeper
+{
+	class Entrypoint
+	{
+		static void Main(string[] args)
+		{
+			// Entrypoint to application
+			Console.WriteLine("Starting up application . . .");
+		}
+	}
+}
