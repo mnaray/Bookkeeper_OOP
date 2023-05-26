cdea1d3119339503b5be9f72b57778507829ead8
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-05-26 10:14:28 +0200

    create error view

---
+++ Bookkeeper/Bookkeeper/View/ErrorView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bookkeeper.View
+{
+    public class ErrorView : IView
+    {
+        private readonly Exception _exception;
+        private readonly string _optionaleNachricht;
+
+        public ErrorView(Exception exception)
+        {
+            _exception = exception;
+            _optionaleNachricht = "Ein Fehler ist aufgetreten!";
+        }
+        
+        public ErrorView(Exception exception, string optionaleNachricht)
+        {
+            _exception = exception;
+            _optionaleNachricht = optionaleNachricht;
+        }
+
+        public void GibAnsichtAus()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ErstelleAnsicht());
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public string ErstelleAnsicht()
+        {
+            string teil1 = Environment.NewLine + "----" + _optionaleNachricht + "----" + Environment.NewLine;
+            string teil2 = _exception.Message + Environment.NewLine;
+            return teil1 + teil2;
+        }
+    }
+}
