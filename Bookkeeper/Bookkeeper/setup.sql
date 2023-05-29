ea61257c1b4c586e8793e36de8530ea081a6fbdc
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-05-29 18:28:22 +0200

    fix typo in setup script

--- Bookkeeper/Bookkeeper/setup.sql
+++ Bookkeeper/Bookkeeper/setup.sql
@@ -23,7 +23,7 @@ CREATE TABLE buchungssaetze (
 );
 
 INSERT INTO bilanz_konto_typen (typ_id, typ_name)
-VALUES (1, 'activ'), (2, 'passiv');
+VALUES (1, 'aktiv'), (2, 'passiv');
 
 INSERT INTO bilanz_konten (konto_id, konto_name, konto_typ)
 VALUES
