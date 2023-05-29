d29b48d818bc7ad90ad21e283ad1ddd9892d6637
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-05-29 19:06:28 +0200

    add description to bilanz command

--- Bookkeeper/Bookkeeper/Controller/Commands/BilanzausgabeCommand.cs
+++ Bookkeeper/Bookkeeper/Controller/Commands/BilanzausgabeCommand.cs
@@ -2,7 +2,14 @@
 {
     public class BilanzausgabeCommand : Command
     {
-        private const string _beschreibung = "beschreibung . . .";
+        private const string _beschreibung =
+            "Dieser Befehl gibt den aktuellen Zustand der Bilanz aus.\n" +
+            "Es werden alle Bilanzkonten aufgelistet.\n" +
+            "Verwende dazu folgende Syntax:\n\n" +
+            "bilanz\n\n" +
+            "Mit einem optionalen Zusatz kann man den Zustand der Bilanz " +
+            "auch vor einem bestimmten Datum ansehen:\n\n" +
+            "bilanz --datum <Datum [TT.MM.JJJJ]>";
 
         public BilanzausgabeCommand()
             : base("bilanz", _beschreibung)
