26558e4d84b9636c8c0c9b98f2531cb21d82927f
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-06-02 09:38:53 +0200

    implement timestamp converter methods

--- Bookkeeper/Bookkeeper/Model/UnixTimestampConverter.cs
+++ Bookkeeper/Bookkeeper/Model/UnixTimestampConverter.cs
@@ -2,14 +2,19 @@
 {
     public class UnixTimestampConverter
     {
-        public DateTime UnixTimestampToDateTime(int date)
+        public static DateTime UnixTimestampToDateTime(int date)
         {
-            throw new NotImplementedException();
+            // The code in this method is from the following source:
+            // https://stackoverflow.com/a/250400/16747568
+
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(date).ToLocalTime();
+            return dateTime;
         }
 
-        public int DateTimeToUnixTimestamp(DateTime date)
+        public static int DateTimeToUnixTimestamp(DateTime date)
         {
-            throw new NotImplementedException();
+            return (int)date.ToUniversalTime().Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
         }
     }
 }
