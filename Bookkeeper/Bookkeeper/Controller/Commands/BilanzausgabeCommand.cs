47d6e6e23414422c270fda3913e1dd0555d32e38
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-06-02 09:01:45 +0200

    implement execution method for bilanzausgabe

--- Bookkeeper/Bookkeeper/Controller/Commands/BilanzausgabeCommand.cs
+++ Bookkeeper/Bookkeeper/Controller/Commands/BilanzausgabeCommand.cs
@@ -1,4 +1,6 @@
-﻿namespace Bookkeeper.Controller.Commands
+﻿using Bookkeeper.View;
+
+namespace Bookkeeper.Controller.Commands
 {
     public class BilanzausgabeCommand : Command
     {
@@ -17,7 +19,39 @@
 
         public override void Ausfuehren(string[] args)
         {
-            throw new NotImplementedException();
+            if (args.Length > 3)
+            {
+                throw new Exception($"Der Befehl <{this.Name}> nimmt nicht mehr als 2 Argumente");
+            }
+
+            if (args.Length == 1) // no date
+            {
+                int jetzt = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+                BilanzView view = new BilanzView(jetzt);
+                view.GibAnsichtAus();
+                return;
+            }
+
+            // with date argument
+            if (args[1] != "--datum")
+            {
+                throw new Exception("Das angegebene Argument ist nicht bekannt.");
+            }
+
+            DateTime datum = DateTime.Now;
+            if (args.Length > 1 && (!DateTime.TryParse(args[2], out datum) || args[2].Length != 12))
+            {
+                throw new Exception("Das angegebene Datum ist nicht im richtigen Format. (TT.MM.JJJJ)");
+            }
+
+            if (datum > DateTime.Now)
+            {
+                throw new Exception("Das Datum muss in der Vergangenheit sein.");
+            }
+
+            int datumUnixTimestamp = (int)datum.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            BilanzView vergangenheitsView = new BilanzView(datumUnixTimestamp);
+            vergangenheitsView.GibAnsichtAus();
         }
     }
 }
