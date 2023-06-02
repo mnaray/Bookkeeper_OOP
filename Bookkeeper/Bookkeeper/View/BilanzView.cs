9b969c4c459b5a26adb0298a0666d78c08db1235
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-06-02 10:14:44 +0200

    implement bilanzView

--- Bookkeeper/Bookkeeper/View/BilanzView.cs
+++ Bookkeeper/Bookkeeper/View/BilanzView.cs
@@ -1,7 +1,11 @@
-﻿namespace Bookkeeper.View
+﻿using Bookkeeper.Model;
+using System.Text;
+
+namespace Bookkeeper.View
 {
     public class BilanzView : IView
     {
+        private const string WAEHRUNG = "CHF";
         private int _datum; // Unix timestamp
 
         /// <summary>
@@ -15,12 +19,46 @@
 
         public void GibAnsichtAus()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(ErstelleAnsicht());
         }
 
         public string ErstelleAnsicht()
         {
-            throw new NotImplementedException();
+            StringBuilder sbResult = new StringBuilder("");
+            StringBuilder sbLine = new StringBuilder("");
+            Bilanz bilanz = new Bilanz();
+            Dictionary<Konto, decimal> kontoSalden = bilanz.StecheBilanz(_datum);
+
+            sbResult.AppendLine($"Bilanz vom {UnixTimestampConverter.UnixTimestampToDateTime(_datum)}");
+
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                sbLine.Append('-');
+            }
+
+            sbResult.AppendLine(sbLine.ToString()); // line of hyphens
+            sbResult.AppendLine(Environment.NewLine);
+            sbResult.AppendLine("=== Aktivkonten ===");
+            sbResult.AppendLine("-------------------");
+
+            bool beiAktivkonten = true;
+            foreach (KeyValuePair<Konto, decimal> saldo in kontoSalden)
+            {
+                // check if at passive part yet
+                if (beiAktivkonten && saldo.Key.KontoId.ToString().StartsWith('2'))
+                {
+                    sbResult.AppendLine(Environment.NewLine);
+                    sbResult.AppendLine("=== Passivkonten ===");
+                    sbResult.AppendLine("--------------------");
+                    beiAktivkonten = false;
+                }
+
+                sbResult.AppendLine(saldo.Key.KontoName);
+                sbResult.AppendLine($"{saldo.Value.ToString()} {WAEHRUNG}");
+                sbResult.AppendLine(Environment.NewLine);
+            }
+
+            return sbResult.ToString();
         }
     }
 }
