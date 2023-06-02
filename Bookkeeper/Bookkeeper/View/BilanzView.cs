d0f358b5b7ae411f725d5816a0ef8bd55f0d5c95
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-06-02 09:03:40 +0200

    extend constructor of bilanzView to take date

--- Bookkeeper/Bookkeeper/View/BilanzView.cs
+++ Bookkeeper/Bookkeeper/View/BilanzView.cs
@@ -2,6 +2,17 @@
 {
     public class BilanzView : IView
     {
+        private int _datum; // Unix timestamp
+
+        /// <summary>
+        /// Konstruktor der BilanzView-Klasse
+        /// </summary>
+        /// <param name="ansichtsdatum">Das Ansichtsdatum ist ein Unix-Timestamp in Form von einem Integer</param>
+        public BilanzView(int ansichtsdatum)
+        {
+            _datum = ansichtsdatum;
+        }
+
         public void GibAnsichtAus()
         {
             throw new NotImplementedException();
